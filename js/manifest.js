32db12090d2e8de50be28e6bb417ee26aa9a6a32
haidan0117 <153364490+haidan0117@users.noreply.github.com>
2025-06-12 02:17:46 +0800

    Add files via upload

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '683ddc626043b69ba3016475';
+_mrmcp['creative_id'] = '6841f867887d6951870077d5';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = '四相护龈录';
+_mrmcp['build_number'] = parseInt('425');
+_mrmcp['publish_time'] = 'Thu Jun 12 2025 02:09:48 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.cn";
+})();
